d2ca40a0c0e8fb3226abbc45ee70a1a905eddc28
Tyler Paiewonsky <tylerpsky@gmail.com>
2025-02-23 23:11:27 -0500

    Task 3 - Created Company Class

--- cc_9.js
+++ cc_9.js
@@ -71,4 +71,66 @@ console.log(mgr1.getDetails());
 
 console.log(mgr1.calculateBonus()); 
 
+//Task 3
 
+class Employee {
+    constructor(name, id, department, salary) {
+        this.name = name;
+        this.id = id;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    getDetails() {
+        return `Employee: ${this.name}, 
+        ID: ${this.id}, 
+        Department: ${this.department}, 
+        Salary: $${this.salary}`;
+    }
+
+    calculateAnnualSalary() {
+        return this.salary * 12;
+    }
+}
+
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary);
+        this.teamSize = teamSize;
+    }
+
+    getDetails() {
+        return `Manager: ${this.name}, 
+        ID: ${this.id}, 
+        Department: ${this.department}, 
+        Salary: $${this.salary}, 
+        Team Size: ${this.teamSize}`;
+    }
+
+    calculateBonus() {
+        return this.calculateAnnualSalary() * 0.1;
+    }
+}
+
+class Company {
+    constructor(name) {
+        this.name = name;
+        this.employees = [];
+    }
+
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+
+    listEmployees() {
+        this.employees.forEach(emp => console.log(emp.getDetails()));
+    }
+}
+
+const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+
+const company = new Company("TechCorp");
+company.addEmployee(emp1);
+company.addEmployee(mgr1);
+company.listEmployees();
