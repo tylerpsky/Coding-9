1357308f70dbc84307257d3b399a872a548614c1
Tyler Paiewonsky <tylerpsky@gmail.com>
2025-02-23 23:05:38 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -25,4 +25,50 @@ console.log(emp1.getDetails());
 
 console.log(emp1.calculateAnnualSalary()); 
 
+//Task 2
+
+class Employee {
+    constructor(name, id, department, salary) {
+        this.name = name;
+        this.id = id;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    getDetails() {
+        return `Employee: ${this.name}, 
+        ID: ${this.id}, Department: 
+        ${this.department}, 
+        Salary: $${this.salary}`;
+    }
+
+    calculateAnnualSalary() {
+        return this.salary * 12;
+    }
+}
+
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary);
+        this.teamSize = teamSize;
+    }
+
+    getDetails() {
+        return `Manager: ${this.name}, 
+        ID: ${this.id}, 
+        Department: ${this.department}, 
+        Salary: $${this.salary}, 
+        Team Size: ${this.teamSize}`;
+    }
+
+    calculateBonus() {
+        return this.calculateAnnualSalary() * 0.1;
+    }
+}
+
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+console.log(mgr1.getDetails());
+
+console.log(mgr1.calculateBonus()); 
+
 
